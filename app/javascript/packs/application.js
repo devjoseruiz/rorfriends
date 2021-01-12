d0b8685984a7860fe775bb581abf931586779d86
José Ruiz <7784299-joseruizdev@users.noreply.gitlab.com>
2021-01-12 04:12:14 +0100

    Add Bootstrap through Webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,19 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+// import the bootstrap javascript module
+import "bootstrap"
+
+// import the application.scss created for the bootstrap CSS
+import "../stylesheets/application"
+
+var jQuery = require('jquery')
+
+// include jQuery in global and window scope (so you can access it globally)
+// in your web browser, when you type $('.div'), it is actually refering to global.$('.div')
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
