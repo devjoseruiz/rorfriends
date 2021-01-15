45fede6fb8d16b591e3c89eb812b68186c7da868
José Ruiz <7784299-joseruizdev@users.noreply.gitlab.com>
2021-01-15 03:25:25 +0100

    Make associations between DB tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_12_053200) do
+ActiveRecord::Schema.define(version: 2021_01_15_013118) do
 
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2021_01_12_053200) do
     t.string "phone"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
